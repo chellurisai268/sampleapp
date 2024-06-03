dce37d142e4320c2c5aaee6647fc0c48a38504f5
chellurisai268 <saiclelluri268@gmail.com>
2024-06-03 13:33:56 +0530

    frhkjdk

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Counter from './components';
-import Todolist from './todolist';
-import Parent from './parentcomponent';
-import GrandChild from './grandchild';
-import { useState } from 'react';
-import { MyContext } from './myContext';
-const Mydetails={
-  firstname:"subbu",
-  age:22,
-  place:"velangi"
-}
+import Products from './apiCalls/products';
+
 
 function App() {
-  var [fn,setfn]=useState("Edupoly")
+ 
   return (
-   <MyContext.Provider value={"code your career"}>
-    <div className="border border-2 border-primary p-2 m-2">
-      {/* <Counter></Counter> 
-      <Todolist></Todolist> */}
-      <h1>App:{fn}</h1>
-      <Parent></Parent>
+   
+    <div> 
+      
+      <h1>App</h1>
+      <Products></Products>
       
     </div>
-    </MyContext.Provider>
+    
   );
 }
 
